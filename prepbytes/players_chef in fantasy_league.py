bd7fecdeb6f2250962a8732d3acccec7419562b7
chiraghs <chiraghs1929@gmail.com>
2020-08-02 07:53:56 +0530

    first commit

---
+++ prepbytes/players_chef in fantasy_league.py
@@ -0,0 +1,12 @@
+''' Chef is going to start playing Fantasy Football League (FFL) this season. In FFL, each team consists of exactly 15 players: 2 goalkeepers, 5 defenders, 5 midfielders and 3 forwards. Chef has already bought 13
+
+players; he is only missing one defender and one forward.
+
+There are N
+available players (numbered 1 through N). For each valid i, the i-th player is either a defender or a forward and has a price Pi. The sum of prices of all players in a team must not exceed 100 dollars and the players Chef bought already cost him S
+
+dollars.
+
+Can you help Chef determine if he can complete the team by buying one defender and one forward in such a way that he does not exceed the total price limit? '''
+
+print(2+2)
